1dbd66fd96ecc5072b63b48813ce647ea49d1950
Daniel7398 <62849456+Daniel7398@users.noreply.github.com>
2022-01-23 22:49:30 +0200

    StartOfProject
    Added initial part of my project.

---
+++ Project/Models/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Project.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Models
+{
+    public class AppDbContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>(ur =>
+            {
+                ur.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                ur.HasOne(ur => ur.Role).WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.RoleId);
+                ur.HasOne(ur => ur.User).WithMany(u => u.UserRoles)
+                .HasForeignKey(ur => ur.UserId);
+            });
+        }
+    }
+}
