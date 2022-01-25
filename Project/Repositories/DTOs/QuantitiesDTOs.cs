402b262ff170b3eef3b1f78baadb710aef50f3d2
Daniel7398 <62849456+Daniel7398@users.noreply.github.com>
2022-01-26 01:13:33 +0200

    DTOs
    Added DTOs for entities.

---
+++ Project/Repositories/DTOs/QuantitiesDTOs.cs
@@ -0,0 +1,29 @@
+﻿using Project.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Repositories.DTOs
+{
+    public class QuantitiesDTOs
+    {
+        public int QuantityId { get; set; }
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int Cantitate { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual Cart Cart { get; set; }
+
+        public QuantitiesDTOs(Quantity quantity)
+        {
+            this.QuantityId = quantity.QuantityId;
+            this.Id = quantity.Id;
+            this.CartId = quantity.CartId;
+            this.Cantitate = quantity.Cantitate;
+            this.Product = quantity.Product;
+            this.Cart = quantity.Cart;
+        }
+    }
+}
