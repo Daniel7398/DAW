c4abe14cc7fa9e1b88d4bb11b8b86de53ea6a3b2
Daniel7398 <62849456+Daniel7398@users.noreply.github.com>
2022-02-01 18:17:36 +0200

    1
    1

--- ASP.NET-Project/ASP_Project/AngularPart/src/app/app.component.spec.ts
+++ ASP.NET-Project/ASP_Project/AngularPart/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularPart'`, () => {
+  it(`should have as title 'demoProjectCTI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularPart');
+    expect(app.title).toEqual('demoProjectCTI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularPart app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demoProjectCTI app is running!');
   });
 });
